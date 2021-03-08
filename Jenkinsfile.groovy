5b03b61a0a9308503bb2f4d6395eb4f572a5df0d
Vincent Thiery <vjmthiery@gmail.com>
2021-03-08 14:16:01 +0100

    ci: update Jenkinsfile (#1)

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,31 +2,29 @@
 
 @Library('EdgeLabJenkins@master') _
 
-def app_name = "spring-rest-exception-handler"
-
-def worker_image = "edgelab/jenkins-worker:v11.4.2"
-def flavor = "default"
-
 def version = version()
 def full_workflow = (env.BRANCH_NAME == 'master')
 
-awsDockerNode(app_name, flavor, worker_image) {
+def maven_image = "maven:3.6.1-jdk-8-slim"
 
-    // Common steps for every jobs:
-    stage('Checkout branch') {
-        checkout scm
-    }
+node('default') {
+  // Common steps for every jobs:
+  stage('Checkout branch') {
+    checkout scm
+  }
 
+  docker.image(maven_image).inside {
     stage("Compile and test") {
-        mavenw("versions:set -DnewVersion='${version}'", "clean verify")
+      mavenw("versions:set -DnewVersion='${version}'", "clean verify")
 
-        stage("Publish JUnit test result report") {
-            junit '**/target/surefire-reports/TEST-*.xml'
-            junit testResults: '**/target/failsafe-reports/TEST-*.xml', allowEmptyResults: true
-        }
+      stage("Publish JUnit test result report") {
+        junit '**/target/surefire-reports/TEST-*.xml'
+        junit testResults: '**/target/failsafe-reports/TEST-*.xml', allowEmptyResults: true
+      }
     }
 
     stageWhen('Publish ', full_workflow) {
-        mavenw("versions:set -DnewVersion='${version}'", "deploy -DskipTests -Dgpg.skip -am")
+      mavenw("versions:set -DnewVersion='${version}'", "deploy -DskipTests -Dgpg.skip -am")
     }
+  }
 }
